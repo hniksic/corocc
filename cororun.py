3a7c34dcb3eaccd98c5fbccbb04e9268ca4b6702
Hrvoje Niksic <hniksic@gmail.com>
2018-03-08 22:04:09 +0100

    Revert "Use global (thread-local) state instead of communicating with start()."
    This reverts commit af318f83cdf70a4e0dbb603e64df0045f976a96a.

--- cororun.py
+++ cororun.py
@@ -1,13 +1,8 @@
 import types
-import threading
-
-_local = threading.local()
-_local.current_cont = None
 
 @types.coroutine
 def suspend(fn, *args):
-    cont = _local.current_cont
-    assert cont is not None
+    cont = yield
     fn(cont, *args)
     cont_retval = yield
     return cont_retval
@@ -15,9 +10,9 @@ def suspend(fn, *args):
 class suspending:
     __slots__ = ('_cont',)
 
-    async def __aenter__(self):
-        cont = _local.current_cont
-        assert cont is not None
+    @types.coroutine
+    def __aenter__(self):
+        cont = yield
         self._cont = cont
         return cont
 
@@ -43,41 +38,34 @@ def _resume_catching(coro, val, fut):
         return False
     return True
 
+_unset = object()
+
 def _step(coro, contval, fut):
     if fut is None:
         resume = _resume_simple
     else:
         resume = _resume_catching
-
-    cont_called = False
-    def cont(val=None):
-        nonlocal contval, cont_called
-        contval = val
-        if cont_called:
-            raise RuntimeError("coroutine already continued")
-        cont_called = True
-        if in_step:
-            # cont() invoked from inside suspend - just continue
-            # with the current step
-            contval = val
-        else:
-            # let step resume the coroutine
-            _step(coro, val, fut)
-
-    prev = getattr(_local, 'current_cont', None)
-    _local.current_cont = cont
-    in_step = True
-    try:
-        while True:
-            suspended = resume(coro, contval, fut)
-            if not suspended:
-                return
-            if not cont_called:
-                return
-            cont_called = False
-    finally:
-        _local.current_cont  = prev
-        in_step = False
+    while True:
+        if not resume(coro, contval, fut):
+            return
+        contval = _unset
+        def cont(val=None):
+            nonlocal contval
+            if contval is not _unset:
+                raise RuntimeError("coroutine already resumed")
+            if in_step:
+                # cont() invoked from inside suspend - just continue
+                # with the current step
+                contval = val
+            else:
+                # let step resume the coroutine
+                _step(coro, val, fut)
+        in_step = True
+        if not _resume_simple(coro, cont, None):
+            raise AssertionError("suspend didn't yield")
+        if contval is _unset:
+            in_step = False
+            return
 
 def start(coro, future=None):
     _step(coro, None, future)
