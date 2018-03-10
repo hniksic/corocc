fc6c4bae0e4fb63a0543c1b102c393ca55a3119b
Hrvoje Niksic <hniksic@gmail.com>
2018-03-10 22:22:07 +0100

    Use an actual class for the continuation callable.

--- corocc/_base.py
+++ corocc/_base.py
@@ -1,6 +1,6 @@
 import types
 
-__all__ = 'suspend', 'suspending', 'start',
+__all__ = 'suspend', 'suspending', 'start', 'Continuation',
 
 @types.coroutine
 def suspend(fn, *args):
@@ -44,6 +44,22 @@ def _resume_catching(coro, val, fut):
         return False
     return True
 
+class Continuation:
+    # using __slots__ actually makes a noticable impact on performance
+    __slots__ = ('_invoked', '_can_resume', '_coro', '_fut', 'start_ctx',
+                 '_contval', 'result')
+
+    def __call__(self, val=None):
+        if self._invoked:
+            raise RuntimeError("coroutine already resumed")
+        self._invoked = True
+        self._contval = val
+        if self._can_resume:
+            # resume the coroutine with the provided value
+            _step(self._coro, val, self._fut, self.start_ctx)
+        # if cont() was invoked from inside suspend, do not step,
+        # just continue with the current step and resume there
+
 def _step(coro, contval, fut, start_ctx):
     resume = _resume_simple if fut is None else _resume_catching
 
@@ -51,26 +67,21 @@ def _step(coro, contval, fut, start_ctx):
         if not resume(coro, contval, fut):
             return
 
-        cont_invoked = [False]
-        def cont(val=None, __invoked=cont_invoked):
-            nonlocal contval
-            if __invoked[0]:
-                raise RuntimeError("coroutine already resumed")
-            __invoked[0] = True
-            contval = val
-            if not in_step:
-                # resume the coroutine with the provided value
-                _step(coro, val, fut, start_ctx)
-            # if cont() was invoked from inside suspend, do not step,
-            # just continue with the current step and resume there
+        cont = Continuation()
+        cont._invoked = False
         cont.start_ctx = start_ctx
-
-        in_step = True
+        cont._coro = coro
+        cont._fut = fut
+        cont.start_ctx = start_ctx
+        # prevent Continuation from trying to resume the coroutine
+        # while still running
+        cont._can_resume = False
         if not _resume_simple(coro, cont, None):
             raise AssertionError("suspend didn't yield")
-        if not cont_invoked[0]:
-            in_step = False
+        if not cont._invoked:
+            cont._can_resume = True
             return
+        contval = cont._contval
 
 def start(coro, *, future=None, ctx=None):
     _step(coro, None, future, ctx)
