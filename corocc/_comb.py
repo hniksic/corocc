708079380690e72a369468585de7548c9e163ab1
Hrvoje Niksic <hniksic@gmail.com>
2018-03-11 00:40:20 +0100

    Propagate exception from coroutines passed to gather().

--- corocc/_comb.py
+++ corocc/_comb.py
@@ -20,17 +20,23 @@ async def gather(*coros):
     if not coros:
         return []
     results = {}
-    mutex = threading.Lock()
+    done_mutex = threading.Lock()
     async with suspending() as cont:
         def on_done(fut):
-            with mutex:
-                results[fut._coro] = fut.result()
-                finished = len(results) == len(coros)
-            if finished:
+            try:
+                fut_result = fut.result()
+            except Exception as e:
+                cont.throw(e)
+                return
+            with done_mutex:
+                results[fut._coro] = fut_result
+                all_finished = len(results) == len(coros)
+            if all_finished:
                 cont()
         for coro in coros:
             fut = _make_future()
             start(coro, future=fut)
             fut._coro = coro
             fut.add_done_callback(on_done)
+
     return [results[coro] for coro in coros]
--- test/test_comb.py
+++ test/test_comb.py
@@ -1,4 +1,8 @@
+import pytest
+
 import threading
+import concurrent.futures
+from concurrent.futures import Future
 
 import corocc
 
@@ -20,3 +24,19 @@ def test_gather():
     corocc.start(gather_coro(events.append, done.set))
     done.wait()
     assert events == [[1, 2, 3]]
+
+
+async def raise_coro():
+    await resume_threaded()
+    1/0
+
+
+async def gather_raise_coro():
+    return await corocc.gather(simple_coro(1), simple_coro(2), raise_coro())
+
+def test_gather_raise():
+    future = Future()
+    corocc.start(gather_raise_coro(), future=future)
+    concurrent.futures.wait([future])
+    with pytest.raises(ZeroDivisionError):
+        future.result()
