7ccd5cc28ffc8b4abf60f86c7320823f41591e48
Hrvoje Niksic <hniksic@gmail.com>
2018-03-11 09:09:27 +0100

    Use a utility function for blocking wait.

--- test/test_comb.py
+++ test/test_comb.py
@@ -10,33 +10,30 @@ async def resume_threaded():
     async with corocc.suspending() as cont:
         threading.Thread(target=cont).start()
 
+def _block(coro):
+    # utility function shared by several tests: start the coroutine,
+    # block until it's done, and return the future containing its
+    # result
+    future = Future()
+    corocc.start(coro, future=future)
+    concurrent.futures.wait([future])
+    return future
+
 async def simple_coro(n):
     await resume_threaded()
     return n
 
-async def gather_coro(log, when_done):
-    log(await corocc.gather(simple_coro(1), simple_coro(2), simple_coro(3)))
-    when_done()
-
 def test_gather():
-    events = []
-    done = threading.Event()
-    corocc.start(gather_coro(events.append, done.set))
-    done.wait()
-    assert events == [[1, 2, 3]]
+    future = _block(corocc.gather(simple_coro(1), simple_coro(2), simple_coro(3)))
+    assert future.result() == [1, 2, 3]
 
 
-async def raise_coro():
+async def raise_later_coro():
     await resume_threaded()
     1/0
 
-
-async def gather_raise_coro():
-    return await corocc.gather(simple_coro(1), simple_coro(2), raise_coro())
-
-def test_gather_raise():
-    future = Future()
-    corocc.start(gather_raise_coro(), future=future)
-    concurrent.futures.wait([future])
+def test_gather_raise_later():
+    future = _block(
+        corocc.gather(simple_coro(1), simple_coro(2), raise_later_coro()))
     with pytest.raises(ZeroDivisionError):
         future.result()
